96d4d4dfd2eb658192bc8617203ec7a04de1aa0e
kazu714 <kazuhoh12@gmail.com>
2022-03-10 08:25:07 +0900

    make it better

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,8 @@ class PostsController < ApplicationController
     private
 
     def post_params
-        params.require(:post).permit(:content)
+        params.require(:post).permit(
+            :content, images: []
+        )
     end
 end
